74301b7b820be0b3bb1703fa820aef13ae4e025b
anxu <axu.home@gmail.com>
2017-11-30 09:18:53 +0800

    修改为多线程获取Hbase数据

--- 02.多项检索用时分析/hbases.py
+++ 02.多项检索用时分析/hbases.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import happybase
+import threading
 from time import time
 
 HBASE_HOST = "10.0.3.41"
 HBASE_TABLE = "smartv"
-GROUP_NUMBER = 1000
+GROUP_NUMBER = 10000
+THREAD_GROUP_NUMBER = 10
+
+r = []
 
 
 class Hbase(object):
@@ -13,8 +17,8 @@ class Hbase(object):
 
     def __init__(self, *args, **kwargs):
         super(Hbase, self).__init__(*args, **kwargs)
-        self.hbase_client = happybase.Connection(HBASE_HOST, timeout=600000)
-        self.table = self.hbase_client.table(HBASE_TABLE)
+        # self.hbase_client = happybase.Connection(HBASE_HOST, timeout=600000)
+        # self.table = self.hbase_client.table(HBASE_TABLE)
 
     def grouped_item_by(self, group_number, items):
         """ '项' 分组 """
@@ -33,15 +37,31 @@ class Hbase(object):
 
         return result
 
+    def fetch_group_by_thread(self, item_group):
+        hbase_client = happybase.Connection(HBASE_HOST, timeout=600000)
+        table = hbase_client.table(HBASE_TABLE)
+        result_items = table.rows(item_group)
+        r.extend(result_items)
+
     def fetchs(self, row_keys):
         i = 1
-        result = []
         groups = self.grouped_item_by(GROUP_NUMBER, row_keys)
         for group in groups:
             s = time()
-            result.extend(self.table.rows(group))
+            threads = []
+            thread_groups = self.grouped_item_by(THREAD_GROUP_NUMBER, group)
+            for thread_group in thread_groups:
+                t = threading.Thread(
+                    target=self.fetch_group_by_thread, name="t", args=(thread_group,))
+                t.start()
+                threads.append(t)
+
+            # 等待所有线程跑完
+            for t in threads:
+                t.join()
+
             e = time()
-            print("{}, {}, {}".format(i, len(group), e - s))
+            print("{}, {}, {}".format(i, len(r), e - s))
             i += 1
 
-        return result
+        return r
